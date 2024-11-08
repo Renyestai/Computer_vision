353edae19a2c3c8a71cb4d8a07a503c6e41993d5
RYYSLIN <ryyslinchik@mail.ru>
2024-11-08 21:12:05 +0700

    Версия для работы с мобильными данными и сломанный интерфейс

--- CompVision.py
+++ CompVision.py
@@ -1,52 +1,64 @@
 import tkinter as tk
-from tkinter import filedialog, messagebox, Canvas
+from tkinter import filedialog, messagebox, Canvas,ttk
 import cv2
 from PIL import Image, ImageTk
 from ultralytics import YOLO
-
+from threading import Thread
+import socket
+import subprocess
 # Загружаем модель YOLO
 model = YOLO('weights/best.pt')  # Убедитесь, что путь указан правильно
+
+
 
 def upload_video():
     filepath = filedialog.askopenfilename(
         filetypes=[("Video Files", "*.mp4;*.avi;*.mov")]
     )
     if filepath:
-        # Очистка текстовой области при загрузке нового видео
+        
         text_area.delete(1.0, tk.END)
         play_video(filepath)
 
+
+
 def play_video(video_path):
+    global cap  #  Определяем  cap  как  глобальную  переменную,  чтобы  она  была  доступна  внутри  функции  update_frame
+
     # Захват видео с помощью OpenCV
     cap = cv2.VideoCapture(video_path)
 
     def update_frame():
+        global cap  
         ret, frame = cap.read()
         results = []
 
         if ret:
-            # Конвертируем кадр в формат RGB для отображения в Tkinter
+            
             frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
             if enabled.get():
-                # Запуск модели YOLO для анализа текущего кадра
-                results = model(frame_rgb)
+                
+                def run_yolo():
+                    nonlocal results
+                    results = model(frame_rgb) 
+                    update_logs(results)  
 
-                # Получаем кадр с нанесенными детекциями
+                yolo_thread = Thread(target=run_yolo)
+                yolo_thread.start()
+
+                
+                yolo_thread.join() 
+
+                
                 annotated_frame = results[0].plot()
-                update_logs(results)  # Обновление логов
-
-                # Изменяем размер кадра для отображения в Tkinter
                 annotated_frame = cv2.resize(annotated_frame, (800, 600))
             else:
-                annotated_frame = cv2.resize(frame_rgb,  (800, 600))
+                annotated_frame = cv2.resize(frame_rgb, (800, 600))
 
             img = ImageTk.PhotoImage(Image.fromarray(annotated_frame))
-
-
-            # Обновляем Canvas новым изображением
             canvas.create_image(0, 0, anchor=tk.NW, image=img)
-            canvas.image = img  # Обновляем изображение в зоне Canvas
+            canvas.image = img 
 
             # Продолжаем обновлять кадры
             root.after(20, update_frame)
@@ -57,33 +69,60 @@ def play_video(video_path):
     # Ожидание полной инициализации окна перед началом обновления кадра
     root.after(200, update_frame)
 
+
+
 def detect_damage():
-    # Функция анализа изображения на предмет повреждений
+    
     messagebox.showinfo("Результат", "Анализ завершен!")
 
 def update_logs(results):
-    # Получаем найденные объекты
+    
     detections = results[0].boxes
     logs = ""
 
-    # Собираем информацию о найденных повреждениях
+   
     for detection in detections:
-        if detection.conf >= 0.2:  # Пример порога уверенности
-            class_id = int(detection.cls)  # ID класса
-            confidence = detection.conf.item()  # Уверенность
-            logs += f"Обнаружено: класс {class_id} с уверенностью {confidence:.2f}\n"
+        if detection.conf >= 0.7:  
+            class_id = int(detection.cls)  
+            class_names = ["Предположительно светофоры", "Поврежденные светофоры", "Ямы?", "Знак_Поврежденный", "Знак_Нормальный"]
+            confidence = detection.conf.item()  
+            logs += f"Обнаружен: класс {class_names[class_id]} с уверенностью {confidence:.2f}\n"
 
     # Обновляем виджет Text с логами
     if logs:
         text_area.insert(tk.END, logs)
-        text_area.see(tk.END)  # Прокручиваем текст вниз
+        text_area.see(tk.END)  
+    
+def on_button_click():
+    rtsp_url = entry_rtsp.get()  
+ 
+    if rtsp_url:
+    
+        def play_rtsp_stream():
+            cap = cv2.VideoCapture(rtsp_url)
+            if cap.isOpened():
+                while True:
+                    ret, frame = cap.read()
+                    if not ret:
+                        break
+                    cv2.imshow("RTSP Stream", frame)
+                    if cv2.waitKey(1) & 0xFF == ord('q'):
+                        break
+                cap.release()
+                cv2.destroyAllWindows()
+            else:
+                print("Ошибка открытия потока.")
+
+        thread = Thread(target=play_rtsp_stream)
+        thread.start()
+   
 
 # Создание главного окна
 root = tk.Tk()
 root.title("Обнаружение повреждений на дороге")
 root.geometry("1280x720")
 
-# Кнопка для загрузки видео
+# для загрузки видео
 btn_upload = tk.Button(root, text="Загрузить видео", command=upload_video)
 btn_upload.place(x=20, y=20)
 
@@ -97,12 +136,19 @@ enabled_checkbutton = tk.Checkbutton(text="Включить поиск повр�
 enabled_checkbutton.pack(padx=10, pady=100, anchor=tk.NW)
 
 # Создаем Canvas для отображения видео
-canvas = Canvas(root, width=800, height=600)
-canvas.place(relx=1.0, rely=0.0, anchor=tk.NE)  # Располагаем в окне с отступом
+canvas = Canvas(root, width=800, height=600, state="disabled")
+canvas.place(relx=1.0, rely=0.0, anchor=tk.NE)  
 
-# Создаем виджет Text для отображения логов
+# отображения логов
 text_area = tk.Text(root, height=20, width=40)
-text_area.place(x=20, y=120)  # Позиционируем виджет
+text_area.place(x=20, y=120)  
+
+button_rtsp = tk.Button(root, text="Найти камеру", command=on_button_click)
+button_rtsp.place(x=200, y=20)
+
+entry_rtsp = ttk.Entry(root, width=30)
+entry_rtsp.pack()
+entry_rtsp.place(x=200, y=60)
 
 # Запуск интерфейса
 root.mainloop()
